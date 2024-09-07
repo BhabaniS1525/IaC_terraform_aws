b0d64785ddbf2404c7b1fc112e39552c19ce9e68
BhabaniS1525 <bss.learn.01@gmail.com>
2024-09-07 09:23:02 +0530

    Terraform scripts for AWS infra setup

---
+++ terraform/keypairs.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "vprofilekey" {
+  key_name   = "vprofilekey"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICwXhXSbGmYiPm1Z3TaNgF9N02ac4pdmHUzIOskwm2/0 bhaba@HP15S-EQ0500AU"
+}
